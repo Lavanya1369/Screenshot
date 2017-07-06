bb06a06eac5337f354734a4d98e82753756d528c
lavanya1369 <lavanyareddy1369@gmail.com>
2017-07-06 17:25:56 +0530

    Initial commit

---
+++ app/src/main/java/com/ench/lavanya1369/screenshot/ScreenshotType.java
@@ -0,0 +1,11 @@
+package com.ench.lavanya1369.screenshot;
+
+/**
+ * Created by sonu on 23/03/17.
+ */
+
+/*  ENUM to identify the type of Screenshot  */
+
+public enum ScreenshotType {
+    FULL, CUSTOM;
+}
